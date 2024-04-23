39704a1b6043480cd3ad9d4be869beddb463b450
Dzmitry Artsiushenka <dmitry.artiushenko@gmail.com>
2024-04-23 22:35:45 +0200

    modified TableViewFactoryImpl.java

--- src/main/java/com/artsiushenka/config/SpringConfiguration.java
+++ src/main/java/com/artsiushenka/config/SpringConfiguration.java
@@ -41,41 +41,13 @@ public class SpringConfiguration {
     }
 
     @Bean
-    @Scope("prototype")
-    public TableView<Item> tableView(){
-        TableView<Item> table = new TableView<>();
-        table.setEditable(true);
-        VBox.setVgrow(table, Priority.ALWAYS);
-        HBox.setHgrow(table, Priority.ALWAYS);
-        TableColumn<Item, String> typeCol = new TableColumn(ColumnName.TYPE.getValue());
-        typeCol.setCellValueFactory(new PropertyValueFactory("type"));
-        typeCol.prefWidthProperty().bind(table.widthProperty().multiply(0.1));
-
-        TableColumn<Item, String> itemNameCol = new TableColumn(ColumnName.ITEM_NAME.getValue());
-        itemNameCol.setCellValueFactory(new PropertyValueFactory("itemName"));
-        itemNameCol.prefWidthProperty().bind(table.widthProperty().multiply(0.4));
-
-        TableColumn<Item, String> lmCol = new TableColumn(ColumnName.LAST_MODIFIED.getValue());
-        lmCol.setCellValueFactory(new PropertyValueFactory("modifiedDateTime"));
-        lmCol.prefWidthProperty().bind(table.widthProperty().multiply(0.4));
-
-        TableColumn<Item, String> sizeCol = new TableColumn(ColumnName.SIZE.getValue());
-        sizeCol.setCellValueFactory(new PropertyValueFactory("size"));
-        sizeCol.prefWidthProperty().bind(table.widthProperty().multiply(0.1));
-
-        table.getColumns().addAll(typeCol, itemNameCol, lmCol, sizeCol);
-
-        return table;
-    }
-
-    @Bean
     public GuiService javaFxService(TableViewCollection tableViewList, TableViewFactory tableViewFactory){
         return new JavaFxServiceImpl(tableViewList, tableViewFactory);
     }
 
     @Bean
-    public TableViewFactory tableViewFactory(ApplicationContext context){
-        return new TableViewFactoryImpl(context);
+    public TableViewFactory tableViewFactory(){
+        return new TableViewFactoryImpl();
     }
 
     @Bean
--- src/main/java/com/artsiushenka/factory/TableViewFactoryImpl.java
+++ src/main/java/com/artsiushenka/factory/TableViewFactoryImpl.java
@@ -1,14 +1,41 @@
 package com.artsiushenka.factory;
 
+import com.artsiushenka.enums.ColumnName;
 import com.artsiushenka.model.Item;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
 import javafx.scene.control.TableView;
 
 @RequiredArgsConstructor
 public class TableViewFactoryImpl implements TableViewFactory{
-    private final ApplicationContext applicationContext;
     public TableView<Item> getTableView(){
-        return applicationContext.getBean(TableView.class);
+        TableView<Item> table = new TableView<>();
+        table.setEditable(true);
+        VBox.setVgrow(table, Priority.ALWAYS);
+        HBox.setHgrow(table, Priority.ALWAYS);
+        TableColumn<Item, String> typeCol = new TableColumn(ColumnName.TYPE.getValue());
+        typeCol.setCellValueFactory(new PropertyValueFactory("type"));
+        typeCol.prefWidthProperty().bind(table.widthProperty().multiply(0.1));
+
+        TableColumn<Item, String> itemNameCol = new TableColumn(ColumnName.ITEM_NAME.getValue());
+        itemNameCol.setCellValueFactory(new PropertyValueFactory("itemName"));
+        itemNameCol.prefWidthProperty().bind(table.widthProperty().multiply(0.4));
+
+        TableColumn<Item, String> lmCol = new TableColumn(ColumnName.LAST_MODIFIED.getValue());
+        lmCol.setCellValueFactory(new PropertyValueFactory("modifiedDateTime"));
+        lmCol.prefWidthProperty().bind(table.widthProperty().multiply(0.4));
+
+        TableColumn<Item, String> sizeCol = new TableColumn(ColumnName.SIZE.getValue());
+        sizeCol.setCellValueFactory(new PropertyValueFactory("size"));
+        sizeCol.prefWidthProperty().bind(table.widthProperty().multiply(0.1));
+
+        table.getColumns().addAll(typeCol, itemNameCol, lmCol, sizeCol);
+
+        return table;
     }
 }
